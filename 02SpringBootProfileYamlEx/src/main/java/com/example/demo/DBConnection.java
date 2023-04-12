d301a2967cd70fc2b1838ee2882a544b6a865409
kshithijakappa <kshithijakappa@gmail.com>
2023-04-12 10:38:49 -0400

    added

---
+++ 02SpringBootProfileYamlEx/src/main/java/com/example/demo/DBConnection.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+@ConfigurationProperties(prefix = "my.app")
+public class DBConnection {
+
+	private String host;
+	private Integer port;
+	private String username;
+	private String password;
+}
